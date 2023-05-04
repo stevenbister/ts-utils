921dd325b6afaaf678cb25b1faec90c5efa5eb3b
Steven Bister <steve.bister@openstudycollege.com>
2023-05-04 11:23:02 +0100

    feat: adds test setup

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+    return {
+        test: {
+            globals: true,
+            exclude: ['node_modules'],
+        },
+    };
+});
